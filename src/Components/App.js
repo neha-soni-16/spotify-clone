0b91bf9e207fecf06a4957253e15fdd818ef8906
neha-soni-16 <nehaachi16@gmail.com>
2021-07-16 17:28:26 +0530

    Main Login page UI

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/App.css";
+import Login from "./Login";
+
+function App() {
+    return (
+        <div className="App">
+            {/* Spotify Logo */}
+            {/* Login With Spotify Button  */}
+            <Login />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/Login.css";
+
+function Login() {
+    return (
+        <div className="loginSpotify">
+            {/* Spotigy Logo */}
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/56/Spotify_logo_horizontal_black.jpg" alt="" />
+            {/* Spotify Login  */}
+            <a>Login with spotify</a>
+        </div>
+    );
+}
+
+export default Login;
